1c6cba26a9fc5b7244a3466d3b5d94c734dfa13a
mengtaiming <1017769935@qq.com>
2018-08-30 17:52:32 +0800

    enter

---
+++ src/pages/detail/count.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index-wrap">
+    count
+  </div>
+</template>
+<script ></script>
+<script>
+
+export default {
+  data () {
+    return {
+      initTime:3000,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
